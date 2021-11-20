45d94a2c4f786dc7258602d3afd0f3e98c717aa1
Rupert Ellis Brown <ruperteb@gmail.com>
2021-11-20 18:25:28 +0000

    1st commit

--- pages/api/test.ts
+++ pages/api/test.ts
@@ -3,19 +3,6 @@ import { firebaseAdmin, dbAdmin } from "../../utils/firebaseAdmin"
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
 
-/*
-const admin = require('firebase-admin')
-if (!admin.apps.length) {
-admin.initializeApp({
-    credential: admin.credential.cert({
-        projectId: "brokerxchange-253e7",
-        clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-        privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
-    }),
-})
-} */
-
-
 
 type Data = {
     response: string
@@ -34,9 +21,6 @@ export default function handler(
             })
     
         
-    
-      
-             // set of operations
          .then(() => {
                 res.status(200).json({response: "success"})
                 res.end()
@@ -49,25 +33,6 @@ export default function handler(
             })
     })
     
-
-    
-
-    /* const checkCustomClaims = async () => {
-
-        await firebaseAdmin.auth().getUser("hHXGoLUOSMgtstnrJBcQnWQzTMj1").then((userRecord: any) => {
-    
-            if (userRecord.customClaims) {
-                console.log(userRecord.customClaims);
-            }
-
-        });
-    }
-
-    checkCustomClaims() */
-
-    /* console.log(req.body);
-
-    res.status(200).json({ response: req.body }) */
 }
 
 
