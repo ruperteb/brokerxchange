9857cbd5aa97f7b0a43e27cb86c13372eded5eab
Rupert Ellis Brown <ruperteb@gmail.com>
2021-11-20 16:47:14 +0000

    test

--- pages/api/test.ts
+++ pages/api/test.ts
@@ -11,7 +11,7 @@ admin.initializeApp({
         clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
         privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
     }),
-}, "adminApp")
+})
 }
 
 
